77226c3afb021487cfc40e4db6c91b15902a4cc5
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-20 11:52:47 +0100

    Java Doc for game

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,22 +1,39 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Game {
     private AbstractFactory abstractFactory;
+    private ArrayList<Entity> entities;
 
     public Game(AbstractFactory abstractFactory) {
         this.abstractFactory = abstractFactory;
     }
 
+    /**
+     * Call this method to start the game
+     */
     public void start() {
         initialize();
 //        TODO loadLevel();
         gameLoop();
     }
 
+    /**
+     * Initialize all the settings of the game.
+     * Makes a call to the AbstractFactory InitializeMethod
+     */
     public void initialize() {
-
+        abstractFactory.initialize();
     }
 
+    /**
+     * execute the game loop.
+     * 1 Get all the inputs and events.
+     * 2 Call the update method on all Entities.
+     * 3 Call the visualize method on all Entities.
+     * 4 Call AbstractFactory render method add the end of the loop.
+     */
     public void gameLoop() {
         while(true) {
 
