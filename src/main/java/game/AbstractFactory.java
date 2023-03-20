23c92c7fee0e8532f23dd173775c25a465c41f81
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-20 11:58:18 +0100

    JavaDoc for abstractfactory

--- src/main/java/game/AbstractFactory.java
+++ src/main/java/game/AbstractFactory.java
@@ -1,10 +1,22 @@
 package game;
 
+/**
+ * Abstract class that defines the Factory that makes the objects needed for the game;
+ */
 public abstract class AbstractFactory {
     public  AbstractFactory() {
 
     }
 
+    /**
+     * Initializes the fields and settings of the factory.
+     * Called once when Game.start() is called.
+     */
     public abstract void initialize();
+
+    /**
+     * Push to completed frame to the user.
+     * Is called at the end of every gameLoop.
+     */
     public abstract void render();
 }
