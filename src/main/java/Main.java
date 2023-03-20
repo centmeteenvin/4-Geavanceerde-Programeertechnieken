c0a6942a7ebc4b86a822836a9881569cabd35185
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-20 11:43:09 +0100

    code in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import game.AbstractFactory;
+import game.Game;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello World");
+        AbstractFactory abstractFactory = null;
+        Game game = new Game(abstractFactory);
+//        TODO game.start();
     }
 }
