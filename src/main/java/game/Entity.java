ab87d45ae9608c80a4ba5352009271eae8808eda
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-20 12:01:05 +0100

    Entity scaffold + javaDoc

--- src/main/java/game/Entity.java
+++ src/main/java/game/Entity.java
@@ -1,2 +1,23 @@
-package game;public class Entity {
+package game;
+
+import java.awt.Point;
+
+/**
+ * Base abstract class for all the things that can move and/or be interacted with.
+ */
+public abstract class Entity {
+    /**
+     * All Entities have a coordinate field to track its position.
+     */
+    private Point coordinate;
+
+    /**
+     * Called after the updates are gathered
+     */
+    public abstract void update();
+
+    /**
+     * Called after updates.
+     */
+    public abstract void visualize();
 }
