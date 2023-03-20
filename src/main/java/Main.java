f3102963456bf85063b4799a97b65a1b04fc0215
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-20 14:42:02 +0100

    Fixed imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
-import game.AbstractFactory;
-import game.Game;
+import game.*;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello World");
         AbstractFactory abstractFactory = null; //Give a concrete implementation of the AbstractFactory
         Game game = new Game(abstractFactory); //Pass it into a new gameObject
-//        TODO game.start(); // Call start method and the game starts playing
+        game.start(); // Call start method and the game starts playing
     }
 }
