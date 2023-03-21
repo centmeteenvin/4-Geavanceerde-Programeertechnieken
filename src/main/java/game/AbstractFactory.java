51633a389ce3e1d0f78ab492536fdca8feb46b8d
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-21 21:52:23 +0100

    Constructor overloading for Abstract Factory + JavaDoc

--- src/main/java/game/AbstractFactory.java
+++ src/main/java/game/AbstractFactory.java
@@ -1,6 +1,7 @@
 package game;
 
 import game.entity.Enemy;
+import game.entity.Entity;
 import game.entity.Player;
 import game.utilities.GameState;
 import game.utilities.Settings;
@@ -8,30 +9,75 @@ import game.utilities.Settings;
 import java.awt.*;
 import java.util.ArrayList;
 
+
 /**
- * Abstract class that defines the Factory that makes the objects needed for the game;
+ * Abstract class that defines the Factory that makes the objects needed for the game.
+ * <p>
+ * This class should be extended for a concrete implementation and passed to Game constructor.
  */
 public abstract class AbstractFactory {
-    protected GameState gameState;
-    protected Settings settings;
-    public  AbstractFactory() {
 
+    /**
+     * The current GameState of the game.
+     * <p>
+     * Is created or passed in {@link #AbstractFactory() }  constructor.<br>
+     * State should be set in {@link #initialize()}.<br>
+     * Game also holds a reference of this object.<br>
+     * Should als be passed to the {@link Player} object because they need to interact with it.<br>
+     */
+    protected GameState gameState;
+
+    /**
+     * The settings for the game.
+     * <p>
+     * Is created or passed in {@link #AbstractFactory()} contructor.<br>
+     * Settings should be set in {@link #initialize()}.<br>
+     * Game also holds a reference of this object.<br>
+     */
+    protected Settings settings;
+
+    /**
+     * Default constructor.
+     * <p>
+     * {@link #gameState} and {@link #settings} are initialized empty.<br>
+     */
+    public AbstractFactory() {
+        this.gameState = new GameState();
+        this.settings = new Settings();
+    }
+
+    /**
+     * Overloaded constructor with parameters.
+     * <p>
+     * {@link #gameState} and {@link #settings} are passed by reference from the parameters.<br>
+     * @param gameState reference to a GameState object.
+     * @param settings reference to a Settings object.
+     */
+    public AbstractFactory(GameState gameState, Settings settings) {
+        this.gameState = gameState;
+        this.settings = settings;
     }
 
     /**
      * Initializes the fields and settings of the factory.
-     * Called once when Game.start() is called.
+     * <p>
+     * Called once when Game.start() is called.<br>
+     * Use this to initialize {@link #gameState} and {@link #settings}.<br>
      */
     public abstract void initialize();
 
     /**
      * Push to completed frame to the user.
-     * Is called at the end of every gameLoop.
+     * <p>
+     * Is called at the end of every gameLoop.<br>
+     * Use this when implementing a buffer draw scheme.
+     * Draw to the buffer with the {@link Entity#visualize()} method.
+     * Push the frame to the screen with this method.<br>
      */
     public abstract void render();
 
     /**
-     * This factory is called when a level is loaded.
+     * This factory is called when a level is loaded.<br>
      * This should be overwritten returning an Enemy object.
      * @param location Point that defines the starting location.
      * @param health double that defines the starting health.
@@ -42,7 +88,7 @@ public abstract class AbstractFactory {
     public abstract Enemy enemyCreator(Point location, double health, double size, Point bounds);
 
     /**
-     * This factory is called when a level is loaded.
+     * This factory is called when a level is loaded.<br>
      * This should be overwritten returning a Player object.
      * @param location Point that defines the starting location.
      * @param health double that defines the starting health.
@@ -54,8 +100,9 @@ public abstract class AbstractFactory {
 
     /**
      * Is used to create the player.
-     * It's mainly used to parse the parameter list into the needed attributes to create a Player object.
-     * The generated parameters are passed to the playerCreator;
+     * <p>
+     * It's mainly used to parse the parameter list into the needed attributes to create a Player object.<br>
+     * The generated parameters are passed to the playerCreator.<br>
      * @param parameters Is the list of parameters that is read from the level file.
      * @return An instance of Enemy that has the given parameters.
      */
@@ -70,7 +117,8 @@ public abstract class AbstractFactory {
 
     /**
      * Is used to create the enemy.
-     * It's mainly used to parse the parameter list into the needed attributes to create an Enemy.
+     * <p>
+     * It's mainly used to parse the parameter list into the needed attributes to create an Enemy.<br>
      * The generated parameters are passed to the enemyCreator;
      * @param parameters Is the list of parameters that is read from the level file.
      * @return An instance of Enemy that has the given parameters.
