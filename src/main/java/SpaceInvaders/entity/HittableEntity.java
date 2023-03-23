cbbe93613d41b2abb54f56d9ca524f1121b3fccf
vincent verbergt <vincent.verbergt@gmail.com>
2023-03-23 14:30:33 +0100

    JavaDoc For Hittable enemy

--- src/main/java/SpaceInvaders/entity/HittableEntity.java
+++ src/main/java/SpaceInvaders/entity/HittableEntity.java
@@ -2,17 +2,40 @@ package SpaceInvaders.entity;
 
 import java.awt.*;
 
+/**
+ * An Extension of the {@link Entity} class for Object that can be hit.
+ * The Hittable Entity Responsible for collision detection.
+ */
 public abstract class HittableEntity extends Entity {
+    /**
+     * The health of an entity.
+     * Every time a HittableEntity gets hit, the health goes down by one.
+     */
     protected double health;
+
+    /**
+     * The size of An Entity
+     * If the distance to the position of //TODO link to bullet is is smaller than the size.<br>
+     * The HittableEntity gets hit.
+     */
     protected double size;
 
+    /**
+     * Default HittableEntity Constructor
+     * @param location {@link Entity#coordinate}
+     * @param health {@link #health}
+     * @param size {@link #size}
+     */
     public HittableEntity(Point location, double health, double size) {
         super(location);
         this.health = health;
         this.size = size;
     }
 
-    public void getHit() {
+    //TODO JavaDoc
+    private void getHit() {
         health--;
     }
+
+    //TODO Hitbox detection when bullet class is made
 }
